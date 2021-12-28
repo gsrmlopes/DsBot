d4aaede82bc846aa452a633aa65de36a87c77da0
gsrmlopes <68873892+gsrmlopes@users.noreply.github.com>
2021-12-27 22:28:24 -0300

    update comando Roll(r)
    agora comando roll suporta multiplas formas de ser chamado

--- commands/r.js
+++ commands/r.js
@@ -1,3 +1,5 @@
+const { devNull } = require("os");
+
 module.exports = {
     name: 'r',
     description: "Roda um Dadado com argumentos",
@@ -7,58 +9,37 @@ module.exports = {
         dado = 1;
         soma = 0;
         multiplica = 1;
-        if(!args[0])return message.reply("use: $r [número de rodadas]#[quantidade de dados]d[valor do dado]+[soma ao valor rolado] [multiplicaçao do resultado]\n\nExemplo: $rolada 3#1d10+4\n\nOu com multiplicação no final, exemplo: 1#1d3+0 3")
-        tem_complemento= false
+        if(!args[0])return message.reply("usos: $r 1d10\n$r 10#1d10\n$r 1d10+5\n$r 10#1d10+5\n$r 10#1d10\n$r 10#1d10 2\n$r 10#1d10+5 2\n$r [qnt rol]#[qnt dados]d[max dado]+[soma por rol] [multip. por rol]")
         base = args[0]
-        if (args[1]!= null){
-            complementp = args[1]
-            tem_complemento = true
-        }
-        if (tentativas == base.split('#')[0] >75 || roladas == base.split('#')[1].split('d')[0]>=50 || dado == base.split('d')[1].split('+')[0] -1 >=100 ||base.split('+')[1]>=100 || multiplica == args[1]>15) return message.reply("ERRO! dado alto de mais!");
-      try {
-        if(tem_complemento == false){
-            tentativas = base.split('#')[0];
-            roladas = base.split('#')[1].split('d')[0];
-            dado = base.split('d')[1].split('+')[0]
-            if (base.split('+')[1] == null){
-                soma = 0;
-            } else {
-                soma = base.split('+')[1];
+        try {
+            base = args[0]
+            if (base.includes("#")){
+                tentativas = base.split('#')[0];
+                roladas = base.split('#')[1].split('d')[0];
             }
-            multiplica = 1;
+            else {
+                tentativas = 1;
+                roladas = base.split('d')[0];
+            }
+            if (base.includes("+")){
+                dado = base.split('d')[1].split('+')[0]
+                soma = parseInt(base.split('+')[1]);
+            } else{
+                dado = base.split('d')[1];
+                soma = 0;
+            }
+            if(!args[1]){
+                multiplica = 1
+            } else{
+                multiplica = parseInt(args[1])
+            }
+            if(tentativas>75 || roladas>50 || dado -1 >99|| soma>99 ||multiplica >15 ) return message.channel.send("ERRO! Dado alto demais!");
             array = [];
             for (j =0;j<tentativas;j++){
                 arrayRolada = [];
                 valorRolada =  0;
                 for (i =0;i<roladas;i++){
                     rolada = Math.floor(Math.random() *dado)+(1+parseInt(soma));
-                    console.log(rolada)
-                    arrayRolada.push(rolada);
-                }
-                for(var i = 0; i < arrayRolada.length; i++) {
-                    valorRolada += arrayRolada[i];
-                }
-            array.push("` "+valorRolada+" ` <= "+arrayRolada.join())
-            }
-            chamada = "("+tentativas + " " + roladas +"d"+(dado+1)+"+"+soma+")*"+multiplica+"\n"
-            message.channel.send(chamada+array.join("\n"));
-        } 
-        else {
-            tentativas = base.split('#')[0];
-            roladas = base.split('#')[1].split('d')[0];
-            dado = base.split('d')[1].split('+')[0] -1
-            if (base.split('+')[1] == null){
-                soma = 0;
-            } else {
-                soma = base.split('+')[1];
-            }
-            multiplica = args[1];
-            array = [];
-            for (j =0;j<tentativas;j++){
-                arrayRolada = [];
-                valorRolada =  0;
-                for (i =0;i<roladas;i++){
-                    rolada = Math.floor(Math.random() *dado)+(2+parseInt(soma));
                     arrayRolada.push(rolada);
                 }
                 for(var i = 0; i < arrayRolada.length; i++) {
@@ -66,11 +47,11 @@ module.exports = {
                 }
             array.push("` "+(valorRolada * multiplica)+" ` <= "+arrayRolada.join())
             }
-            chamada = "("+tentativas + " (" + roladas +"d"+(dado+1)+"+"+soma+")*"+multiplica+"\n"
+            //chamada = "("+tentativas + " (" + roladas +"d"+(dado+1)+"+"+soma+")*"+multiplica+"\n"
+            chamada = " (" + roladas +"d"+(dado)+"+"+soma+")*"+multiplica+"\n"
             message.channel.send(chamada+array.join("\n"));
+        } catch(e){
+            console.log(e);
         }
-    } catch(e) {
-        message.channel.send("Uso incorreto!");
     }
-}
 }
